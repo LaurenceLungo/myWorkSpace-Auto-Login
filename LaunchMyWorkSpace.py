28c43b08741ff242e12dab1073f6fdc50764fd34
Laurence Ng <npn5000@gmail.com>
2022-10-08 00:09:51 +0800

    wait for elements to appear before manipulation

--- LaunchMyWorkSpace.py
+++ LaunchMyWorkSpace.py
@@ -1,4 +1,6 @@
 import undetected_chromedriver as webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import argparse
 import os
@@ -18,10 +20,11 @@ if PASSCODE is None:
 driver = webdriver.Chrome(use_subprocess=True)
 driver.get(os.getenv('MYWORKSPACE_URL'))
 
-driver.find_element(By.ID, "login").send_keys(os.getenv('USER_NAME'))
-driver.find_element(By.ID, "passwd1").send_keys(os.getenv('PASSWORD'))
-driver.find_element(By.ID, "passwd").send_keys(PASSCODE)
-driver.find_element(By.ID, "loginBtn").click()
+wait = WebDriverWait(driver, 10)
+wait.until(EC.visibility_of_element_located((By.ID, "login"))).send_keys(os.getenv('USER_NAME'))
+wait.until(EC.visibility_of_element_located((By.ID, "passwd1"))).send_keys(os.getenv('PASSWORD'))
+wait.until(EC.visibility_of_element_located((By.ID, "passwd"))).send_keys(PASSCODE)
+wait.until(EC.visibility_of_element_located((By.ID, "loginBtn"))).click()
 
 while True:
     pass
